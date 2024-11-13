27167bd87b16848bfa85962a0432ff574f40ba58
default <aroa.perez2@educa.madrid.org>
2024-11-13 17:30:54 +0000

    practica nueva php

--- practica01/mazmorra.php
+++ practica01/mazmorra.php
@@ -1,32 +1,22 @@
 <html>
 <head>
     <style>
-            table {
+            table, td  {
                 border-collapse: collapse;
                 padding: 0;
-                border: 0;
-            }
-            td, tbody {
-                padding: 0;
-                border: none;
-                border-collapse: collapse;
             }
     </style>
 </head>
 <body>
-    
 <table>
     <?php
-
         $array = [
             ["E", "E", "E", "E"],
             ["E", "ETE", "E", "E"],
             ["ET", "TIE", "E", "ET"],
             ["EI", "IT", "T", "TI"]   
         ];
-
         function mazmorra($tiles) {
-            
             foreach($tiles as $tile){
                 echo "<tr>" ;
                 foreach($tile as $imagen) {
@@ -37,7 +27,6 @@
                 echo "</tr>" ;
             }
         }
-
     mazmorra($array);
     ?>
 </table>
